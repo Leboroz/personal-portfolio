1f840d95970f0e0409aee078ae20e4d24c0b4dc8
Leonardo Albornoz <albornozleo95@gmail.com>
2021-12-13 16:22:03 -0400

    Add close button
    I wrapped the image in the menu with a span. I added an id and a class to the span to style and connect to the js file.
    Also, I added an event listner to the span to close the mobile navbar.

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
-window.onload = function (){
+window.onload = function () {
   let hamburgerMenu = document.querySelector('#hamburger');
   let menu = document.querySelector('#menu');
+  let closeMenu = document.querySelector('#closeMenu');
   //console.log(hamburgerMenu);
-  hamburgerMenu.addEventListener('click',function(e){
-    menu.classList.add('visible-menu');
-  });
+  let openAndClose = function () {
+    menu.classList.toggle('visible-menu');
+  }
+  hamburgerMenu.addEventListener('click', openAndClose);
+  closeMenu.addEventListener('click', openAndClose)
 }
