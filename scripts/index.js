c914c23c4f96f3bb439882e94b24f69a26cfa0bb
Carlos Herver <carlos.h.s.1994@gmail.com>
2021-12-13 13:58:49 -0600

    Add event listener to the hamburger menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,8 @@
-for (let i = 0; i < 10; i += 1) {
+window.onload = function (){
+  let hamburgerMenu = document.querySelector('#hamburger');
+  let menu = document.querySelector('#menu');
+  //console.log(hamburgerMenu);
+  hamburgerMenu.addEventListener('click',function(e){
+    menu.classList.add('visible-menu');
+  });
 }
