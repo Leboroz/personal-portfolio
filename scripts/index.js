c9aaa4bf3596a5e5430c32037a8623eeb1f8af8b
Carlos Herver <carlos.h.s.1994@gmail.com>
2021-12-13 14:48:16 -0600

    Add the event listener to the menu buttons

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,14 @@ window.onload = function () {
   let hamburgerMenu = document.querySelector('#hamburger');
   let menu = document.querySelector('#menu');
   let closeMenu = document.querySelector('#closeMenu');
-  //console.log(hamburgerMenu);
+  let closeNavLink = document.querySelectorAll('li.nav-link a');
   let openAndClose = function () {
     menu.classList.toggle('visible-menu');
   }
+  console.log(closeNavLink);
   hamburgerMenu.addEventListener('click', openAndClose);
-  closeMenu.addEventListener('click', openAndClose)
+  closeMenu.addEventListener('click', openAndClose);
+  closeNavLink.forEach(element => {
+    element.addEventListener('click', openAndClose);
+  });
 }
