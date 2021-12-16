a434f8129819ba5552fa07a45c496c494e74813c
Carlos Herver <carlos.h.s.1994@gmail.com>
2021-12-16 13:34:33 -0600

    Add verification for local storage

--- scripts/index.js
+++ scripts/index.js
@@ -166,4 +166,45 @@ window.onload = function () {
       e.preventDefault();
     }
   });
+
+  function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+  }
+
+  const userName = document.getElementById('name');
+  const textArea = document.getElementById('message');
+  const fields = { name: String, email : String, textArea: String };
+
+  if (storageAvailable('localStorage')) {
+    if (localStorage.getItem('fields')) {
+      fields = JSON.parse(localStorage.getItem('fields'));
+      userName.value = fields.name;
+      email.value = fields.email;
+      textArea.value = fields.textArea;
+    }
+  }
+  else {
+    console.log('Local Storage not available');
+  }
 };
