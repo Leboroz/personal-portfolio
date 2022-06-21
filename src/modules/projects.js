04910005ee65d6f831237139ca8d6543b9baf162
Leonardo Albornoz <albornozleo95@gmail.com>
2022-06-20 21:30:46 -0400

    Add new projects

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -8,21 +8,21 @@ import project6 from '../assets/images/projects/project-6.png';
 const arrOfProjects = [
   {
     image: project1,
-    title: 'Ballhead',
-    languages: ['React-Redux', 'SASS', 'Javascript'],
+    title: 'RentUrTech',
+    languages: ['React-Redux', 'SASS', 'Ruby on Rails'],
     description:
-      'this is a design for a mobile app that fetches from an pollution API and displays the data. Built with React-Redux and SASS.',
-    linkLiveVersion: 'https://leboroz-ballhead.netlify.app/',
-    linkToSource: 'https://github.com/Leboroz/ballhead-app',
+      'This is a React application part of a full-stack project called "RentUrTech". The main goal of this project is to provide users with an environment to reserve a specific product for a set amount of time. Thus, in this application users can create an account, can log in, and can see all the added equipment or the details of one. They will be required to log in to manage their own reservations. For creating and destroying equipment, the user needs a proper role. Feel free to take this project and modify it for your own business use!',
+    linkLiveVersion: 'https://renturtech.netlify.app/',
+    linkToSource: 'https://github.com/Final-Capstone-Elite-Group/RentUrTechFrontend',
   },
   {
     image: project2,
-    title: 'Bookstore',
-    languages: ['React', 'Bootstrap', '+1'],
+    title: 'Budget app',
+    languages: ['Ruby on rails', 'Bootstrap', '+1'],
     description:
-      'This is a web App that displays a list of books and fetches an API. Built with React, Redux, Typescript, SASS.',
-    linkLiveVersion: 'https://bookstore-leboroz.herokuapp.com/',
-    linkToSource: 'https://github.com/Leboroz/bookstore',
+      'This is a ruby on rails application that allows to create users and authenticates them using devise. When users are loged in the can create groups of transactions and see the total amout. Users can create transactions for one or many groups.',
+    linkLiveVersion: 'https://leboroz-budget-app.herokuapp.com/',
+    linkToSource: 'https://github.com/Leboroz/budget-app',
   },
   {
     image: project3,
@@ -35,11 +35,12 @@ const arrOfProjects = [
   },
   {
     image: project4,
-    title: 'To do',
-    languages: ['Javascript', 'HTML', 'SASS'],
-    description: 'This is a JavaScript application of a to do list.',
-    linkLiveVersion: 'https://leboroz.github.io/to-do-list/',
-    linkToSource: 'https://github.com/Leboroz/to-do-list/',
+    title: 'Bookstore',
+    languages: ['Ruby on rails', 'Bootstrap', '+1'],
+    description:
+      'This is a web App that displays a list of books and fetches an API. Built with React, Redux, Typescript, SASS.',
+    linkLiveVersion: 'https://bookstore-leboroz.herokuapp.com/',
+    linkToSource: 'https://github.com/Leboroz/bookstore',
   },
   {
     image: project5,
@@ -50,12 +51,13 @@ const arrOfProjects = [
     linkToSource: 'https://github.com/Leboroz/capstone-project-1',
   },
   {
-    image: project6,
-    title: 'Awesome Books',
-    languages: ['HTML/CSS', 'Javascript', 'SASS'],
-    description: 'This is a javascript book list application',
-    linkLiveVersion: 'https://github.com/Leboroz/awesome-books',
-    linkToSource: 'https://github.com/Leboroz/awesome-books',
+    image: project3,
+    title: 'Space Travelers',
+    languages: ['React-Redux', 'Bootstrap', '+1'],
+    description:
+      'A web application for a company that provides commercial and scientific space travel services. The application allows users to book rockets and join selected space missions using real live data from the SpaceX API.',
+    linkLiveVersion: 'https://space-travelers.herokuapp.com/',
+    linkToSource: 'https://github.com/Leboroz/space-travelers',
   },
 ];
 
