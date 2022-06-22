a6bfe7e54216e51fe643fa034062717df32c8e2c
Leonardo Albornoz <albornozleo95@gmail.com>
2022-06-22 14:14:58 -0400

    Fix grid and Add new projects

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -1,9 +1,9 @@
-import project1 from '../assets/images/projects/project-1.png';
-import project2 from '../assets/images/projects/project-2.png';
-import project3 from '../assets/images/projects/project-3.png';
-import project4 from '../assets/images/projects/project-4.png';
-import project5 from '../assets/images/projects/project-5.png';
-import project6 from '../assets/images/projects/project-6.png';
+import project1 from '../assets/images/projects/project-1.jpg';
+import project2 from '../assets/images/projects/project-2.jpg';
+import project3 from '../assets/images/projects/project-3.jpg';
+import project4 from '../assets/images/projects/project-4.jpg';
+import project5 from '../assets/images/projects/project-5.jpg';
+import project6 from '../assets/images/projects/project-6.jpg';
 
 const arrOfProjects = [
   {
