11b64351fd8f132b48619c77e8938af17334def2
Leonardo Albornoz <albornozleo95@gmail.com>
2022-06-23 15:38:23 -0400

    fix project images

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -26,21 +26,21 @@ const arrOfProjects = [
   },
   {
     image: project3,
+    title: 'Ballhead',
+    languages: ['React-Redux', 'SASS', '+1'],
+    description:
+      'this is a design for a mobile app that fetches from an pollution API and displays the data. Built with React-Redux and SASS.',
+    linkLiveVersion: 'https://leboroz-ballhead.netlify.com ',
+    linkToSource: 'https://github.com/Leboroz/ballhead-app',
+  },
+  {
+    image: project4,
     title: 'Space Travelers',
     languages: ['React-Redux', 'Bootstrap', '+1'],
     description:
       'A web application for a company that provides commercial and scientific space travel services. The application allows users to book rockets and join selected space missions using real live data from the SpaceX API.',
     linkLiveVersion: 'https://space-travelers.herokuapp.com/',
     linkToSource: 'https://github.com/Leboroz/space-travelers',
-  },
-  {
-    image: project4,
-    title: 'Bookstore',
-    languages: ['Ruby on rails', 'Bootstrap', '+1'],
-    description:
-      'This is a web App that displays a list of books and fetches an API. Built with React, Redux, Typescript, SASS.',
-    linkLiveVersion: 'https://bookstore-leboroz.herokuapp.com/',
-    linkToSource: 'https://github.com/Leboroz/bookstore',
   },
   {
     image: project5,
@@ -51,13 +51,13 @@ const arrOfProjects = [
     linkToSource: 'https://github.com/Leboroz/capstone-project-1',
   },
   {
-    image: project3,
-    title: 'Space Travelers',
-    languages: ['React-Redux', 'Bootstrap', '+1'],
+    image: project6,
+    title: 'Bookstore',
+    languages: ['Ruby on rails', 'Bootstrap', '+1'],
     description:
-      'A web application for a company that provides commercial and scientific space travel services. The application allows users to book rockets and join selected space missions using real live data from the SpaceX API.',
-    linkLiveVersion: 'https://space-travelers.herokuapp.com/',
-    linkToSource: 'https://github.com/Leboroz/space-travelers',
+      'This is a web App that displays a list of books and fetches an API. Built with React, Redux, Typescript, SASS.',
+    linkLiveVersion: 'https://bookstore-leboroz.herokuapp.com/',
+    linkToSource: 'https://github.com/Leboroz/bookstore',
   },
 ];
 
